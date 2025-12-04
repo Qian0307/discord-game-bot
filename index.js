e1b69a98b36baec87e2811e4a1f307484ce88279
雙城風月 <cc8576a@gmail.com>
2025-12-04 13:52:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+import {
+  Client,
+  GatewayIntentBits,
+  Routes,
+  SlashCommandBuilder,
+  Collection
+} from "discord.js";
+
+import { REST } from "@discordjs/rest";
+import dotenv from "dotenv";
+dotenv.config();
+
+// 系統模組
+import { startGame } from "./systems/start.js";
+import { handleDungeonAction } from "./systems/dungeon.js";
+import { handleBattleAction } from "./systems/battle.js";
+import { handleEventResult, goToNextFloor } from "./systems/dungeon.js";
+import { handleInventoryAction } from "./systems/inventory.js";
+import { handleSkillMenu } from "./systems/skills.js";
+
+
+// ⭐⭐ 這一行你漏掉了：建立 client ⭐⭐
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+export const players = new Map();
+
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isButton() && !interaction.isChatInputCommand()) return;
 
@@ -67,3 +99,4 @@ client.on("interactionCreate", async (interaction) => {
     return handleInventoryAction(interaction, players, id);
   }
 });
+
